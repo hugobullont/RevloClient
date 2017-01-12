3484bd5ec8869732c7ff71da836dd38b12b1eebd
Hugo <hugobullont@gmail.com>
2017-01-11 21:22:53 -0500

    SongRequests added.

--- RevloClient/SongRequest.xaml.cs
+++ RevloClient/SongRequest.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using TwitchLib;
+using TwitchLib.Events.Client;
 
 namespace RevloClient
 {
@@ -31,7 +33,9 @@ namespace RevloClient
         List<Song> songList;
 
         DispatcherTimer dispatcherTimer;
-        
+        TwitchClient client;
+        RevloRepository revloRepo;
+
         public SongRequest()
         {
             InitializeComponent();
@@ -42,9 +46,10 @@ namespace RevloClient
             this.srRedID = srRedID;
             this.revloKey = revloKey;
             this.ircKey = ircKey;
-            this.chatUsername = chatUsername;
-            this.channel = channel;
+            this.chatUsername = chatUsername.ToLower();
+            this.channel = channel.ToLower();
             this.login = login;
+            revloRepo = new RevloRepository();
             dispatcherTimer = new DispatcherTimer();
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
             dispatcherTimer.Interval = new TimeSpan(0, 0, 30);
@@ -54,15 +59,25 @@ namespace RevloClient
 
         private void SongRequest_Loaded(object sender, RoutedEventArgs e)
         {
+            
             SongRepository songRepo = new SongRepository();
             songList = songRepo.GetSongs(srRedID, revloKey);
-
+            client = new TwitchClient(new TwitchLib.Models.Client.ConnectionCredentials(chatUsername, ircKey), channel);
+            client.OnConnected += clientConnected;
+            client.OnJoinedChannel += clientJoinedChannel;
+            client.OnMessageSent += clientMessageSent;
+            client.Connect();
             songsDataGrid.ItemsSource = songList;
 
             if(songList.Count==0)
             {
                 songsDataGrid.Visibility = Visibility.Collapsed;
                 labelAdvice.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                songsDataGrid.Visibility = Visibility.Visible;
+                labelAdvice.Visibility = Visibility.Collapsed;
             }
         }
 
@@ -76,6 +91,54 @@ namespace RevloClient
             SongRepository songRepo = new SongRepository();
             songList = songRepo.GetSongs(srRedID, revloKey);
             songsDataGrid.ItemsSource = songList;
+
+            if (songList.Count == 0)
+            {
+                songsDataGrid.Visibility = Visibility.Collapsed;
+                labelAdvice.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                songsDataGrid.Visibility = Visibility.Visible;
+                labelAdvice.Visibility = Visibility.Collapsed;
+            }
+
+            SentSongRequests(songList);
+        }
+
+        public void SetLabelConsoleContent(String content)
+        {
+            Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background,
+            new Action(() => this.labelConsole.Content = content));
+        }
+
+        public void clientConnected(object sender, OnConnectedArgs e)
+        {
+            String content = "Client connected to Twitch!";
+            SetLabelConsoleContent(content);
+    
+        }
+
+        public void clientJoinedChannel(object sender, OnJoinedChannelArgs e)
+        {
+
+            String content = "Client joined channel: " +e.Channel;
+            SetLabelConsoleContent(content);
+        }
+
+        public void clientMessageSent(object sender, OnMessageSentArgs e)
+        {
+            String content = "SongRequest sended: " + e.SentMessage.Message;
+            SetLabelConsoleContent(content);
+        }
+
+        public void SentSongRequests(List<Song> songList)
+        {
+            foreach(Song temp in songList)
+            {
+                client.SendMessage("!songs request " + temp.song);
+                revloRepo.SetRedemptionCompleted(revloKey, temp.redemption_id);
+            }
         }
 
     }
